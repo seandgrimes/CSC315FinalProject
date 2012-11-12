f09420e405c2aa14064c9537e5c26bd5bd75f193
Sean Grimes <sean@codemunkey.net>
2012-11-12 14:26:59 -0600

    Initial commit

---
+++ src/csc315finalproject/CSC315FinalProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc315finalproject;
+
+/**
+ *
+ * @author Sean
+ */
+public class CSC315FinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
