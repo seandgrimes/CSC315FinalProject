95122f62874feba261387b672e8926ae93771258
Sean Grimes <sean@codemunkey.net>
2012-11-12 14:51:10 -0600

    Code to read the SQL file and populate the database

--- src/csc315finalproject/CSC315FinalProject.java
+++ src/csc315finalproject/CSC315FinalProject.java
@@ -4,16 +4,62 @@
  */
 package csc315finalproject;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Sean
  */
 public class CSC315FinalProject {
-
+    private String inputFile;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public CSC315FinalProject(String inputFile)
+    {
+        this.inputFile = inputFile;
+    }
+    
+    public void run() throws FileNotFoundException, IOException, SQLException, ClassNotFoundException {
+        createDatabase();
+    }
+    
+    private void createDatabase() throws FileNotFoundException, IOException, SQLException, ClassNotFoundException {
+        // Read the file in
+        StringBuilder buffer = new StringBuilder();
+        FileInputStream stream = new FileInputStream(inputFile);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+            
+        // Create the tables
+        String sql = buffer.toString();
+        Connection conn = getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.execute();
+    }
+    
+    private Connection getConnection() throws SQLException, ClassNotFoundException {
+        // Load the JDBC driver for SQL Server
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        
+        // Connect to the database. We'll just use Windows Security
+        final String url = "jdbc:sqlserver://localhost:1433;databaseName=FinalProject;integratedSecurity=true";
+        return DriverManager.getConnection(url);
     }
 }
