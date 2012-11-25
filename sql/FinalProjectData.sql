5ecc86340d61e71a7629b350bc2a3087867aae4f
Sean Grimes <sean@codemunkey.net>
2012-11-25 13:57:18 -0600

    Adding SQL files for the project

---
+++ sql/FinalProjectData.sql
@@ -0,0 +1,91 @@
+-- Create the Travel Agents
+INSERT INTO Agent (FirstName, LastName) VALUES ('Joe', 'Camel');
+INSERT INTO Agent (FirstName, LastName) VALUES ('Sean', 'Grimes');
+INSERT INTO Agent (FirstName, LastName) VALUES ('Caroline', 'Gingles');
+
+-- Create the reservation statuses
+INSERT INTO ReservationStatus (Name) VALUES ('OK');
+INSERT INTO ReservationStatus (Name) VALUES ('WAITING');
+
+-- Create the airports
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KABI', 'Abilene Regional Airport', 'Abilene, TX', 'United States', '318-222-2222')
+
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KDAL', 'Dallas Love Field', 'Dalls, TX', 'United States', '318-222-2222');
+
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KDFW', 'Dallas-Fort Worth International Airport', 'Fort Worth, TX', 'United States', '318-222-2222');
+
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KDEN', 'Denver International Airport', 'Denver, CO', 'United States', '318-222-2222');
+
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KPHX', 'Phoenix Sky Harbor International Airport', 'Phoenix, AZ', 'United States', '318-222-2222');
+
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KATL', 'Hartsfield-Jackson Atlanta International Airport', 'Atlanta, GA', 'United States', '318-222-2222');
+
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KBFI', 'Boeing Field/King County International Airport', 'Seattle, WA', 'United States', '318-222-2222');
+
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KBNA', 'Nashville International Airport', 'Nashville, TN', 'United States', '318-222-2222');
+
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KBOS', 'Logan International Airport', 'Boston, MA', 'United States', '318-222-2222');
+	
+INSERT INTO Airport(InternationalCode, Name, City, Country, PhoneNumber)
+	VALUES ('KLAX', 'Los Angeles International Airport', 'Los Angeles, CA', 'United States', '318-222-2222');
+
+-- Create Carriers
+INSERT INTO Carrier (Name, Address, City, State, Country, PhoneNumber) VALUES ('American Airlines', '123 Test Lane',
+	'Dallas', 'TX', 'United States', '469-569-8253');
+	
+INSERT INTO Carrier (Name, Address, City, State, Country, PhoneNumber) VALUES ('Delta Airlines', '123 Test Lane',
+	'Dallas', 'TX', 'United States', '469-569-8253');
+	
+INSERT INTO Carrier (Name, Address, City, State, Country, PhoneNumber) VALUES ('Southwest Airlines', '123 Test Lane',
+	'Dallas', 'TX', 'United States', '469-569-8253');
+	
+-- Create Payment Types
+INSERT INTO PaymentType (Type) VALUES ('Credit Card');
+INSERT INTO PaymentType (Type) VALUES ('Check');
+
+-- Create Preferences
+INSERT INTO Preference (Name) VALUES ('Window Seat');
+INSERT INTO Preference (Name) VALUES ('Meal');
+INSERT INTO Preference (Name) VALUES ('Aisle Seat');
+
+-- Create the Airplanes
+INSERT INTO Airplane([Identity], Type, BusinessCapacity, EconomyCapacity) VALUES ('Boeing', 'Jet', 50, 200);
+INSERT INTO Airplane([Identity], Type, BusinessCapacity, EconomyCapacity) VALUES ('Airbus', 'Jet', 75, 225);
+INSERT INTO Airplane([Identity], Type, BusinessCapacity, EconomyCapacity) VALUES ('Embrair', 'Turboprop', 10, 75);
+
+DECLARE @American VARCHAR(45);
+DECLARE @Delta VARCHAR(45);
+DECLARE @Southwest VARCHAR(45);
+DECLARE @Boeing INT;
+DECLARE @Airbus INT;
+
+SELECT @American = Name FROM Carrier WHERE Name LIKE 'American%';
+SELECT @Delta = Name FROM Carrier WHERE Name LIKE 'Delta%';
+SELECT @Southwest = Name FROM Carrier WHERE Name LIKE 'Southwest%';
+SELECT @Boeing = AirplaneId FROM Airplane WHERE [Identity] = 'Boeing';
+SELECT @Airbus = AirplaneId FROM Airplane WHERE [Identity] = 'Airbus';
+
+INSERT INTO Flight (FlightNumber, DepartureAirportCode, ArrivalAirportCode, AirplaneId, CarrierName,
+	Date, Fare, DepartureTime, ArrivalTime) VALUES ('SW-12345', 'KDFW', 'KLAX', @Airbus, @Southwest,
+	'12/31/2006', 400, '12:00PM' ,'3:00PM');
+	
+INSERT INTO Flight (FlightNumber, DepartureAirportCode, ArrivalAirportCode, AirplaneId, CarrierName,
+	Date, Fare, DepartureTime, ArrivalTime) VALUES ('AA-12345', 'KDFW', 'KLAX', @Boeing, @American,
+	'12/31/2006', 400, '8:00AM' ,'11:00AM');
+
+INSERT INTO Flight (FlightNumber, DepartureAirportCode, ArrivalAirportCode, AirplaneId, CarrierName,
+	Date, Fare, DepartureTime, ArrivalTime) VALUES ('DA-12345', 'KDFW', 'KLAX', @Boeing, @Delta,
+	'12/31/2006', 400, '3:00PM' ,'6:00PM');
+	
+INSERT INTO Flight (FlightNumber, DepartureAirportCode, ArrivalAirportCode, AirplaneId, CarrierName,
+	Date, Fare, DepartureTime, ArrivalTime) VALUES ('DA-23456', 'KDFW', 'KLAX', @Boeing, @Delta,
+	'12/31/2006', 400, '7:00PM' ,'10:00PM');
---
+++ sql/FinalProjectSchema.sql
@@ -0,0 +1,198 @@
+IF OBJECT_ID('dbo.Flight_Reservation') IS NOT NULL
+BEGIN
+	DROP TABLE Flight_Reservation;
+END;
+
+IF OBJECT_ID('dbo.Reservation') IS NOT NULL
+BEGIN
+	DROP TABLE Reservation;
+END;
+
+IF OBJECT_ID('dbo.Flight') IS NOT NULL
+BEGIN
+	DROP TABLE Flight;
+END;
+
+IF OBJECT_ID('dbo.FrequentFlyer') IS NOT NULL
+BEGIN
+	DROP TABLE FrequentFlyer;
+END;
+
+IF OBJECT_ID('dbo.PaymentMethod') IS NOT NULL
+BEGIN
+	DROP TABLE PaymentMethod;
+END;
+
+IF OBJECT_ID('dbo.Customer') IS NOT NULL
+BEGIN
+	DROP TABLE Customer;
+END; 
+
+IF OBJECT_ID('dbo.ReservationPreference') IS NOT NULL
+BEGIN
+	DROP TABLE ReservationPreference;
+END;
+
+IF OBJECT_ID('dbo.Preference') IS NOT NULL
+BEGIN
+	DROP TABLE Preference;
+END;
+
+IF OBJECT_ID('dbo.PaymentType') IS NOT NULL
+BEGIN
+	DROP TABLE PaymentType;
+END;
+
+IF OBJECT_ID('dbo.ReservationStatus') IS NOT NULL
+BEGIN
+	DROP TABLE ReservationStatus;
+END;
+
+IF OBJECT_ID('dbo.Agent') IS NOT NULL
+BEGIN
+	DROP TABLE Agent;
+END;
+
+IF OBJECT_ID('dbo.Airplane') IS NOT NULL
+BEGIN
+	DROP TABLE Airplane;
+END;
+
+IF OBJECT_ID('dbo.Carrier') IS NOT NULL
+BEGIN
+	DROP TABLE Carrier;
+END;
+
+IF OBJECT_ID('dbo.Airport') IS NOT NULL
+BEGIN
+	DROP TABLE Airport;
+END;
+
+CREATE TABLE Airport
+(
+	InternationalCode VARCHAR(5) NOT NULL PRIMARY KEY,
+	Name VARCHAR(255) NOT NULL,
+	City VARCHAR(45) NOT NULL,
+	Country VARCHAR(45) NOT NULL,
+	PhoneNumber VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Carrier
+(
+	Name VARCHAR(45) NOT NULL PRIMARY KEY,
+	Address VARCHAR(45) NOT NULL,
+	City VARCHAR(45) NOT NULL,
+	State VARCHAR(45) NOT NULL,
+	Country VARCHAR(45) NOT NULL,
+	PhoneNumber VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Airplane
+(
+	AirplaneID INT NOT NULL IDENTITY PRIMARY KEY,
+	[Identity] VARCHAR(10) NOT NULL,
+	[Type] VARCHAR(10) NOT NULL,
+	BusinessCapacity INT NOT NULL,
+	EconomyCapacity INT NOT NULL
+);
+
+CREATE TABLE Agent
+(
+	Id INT IDENTITY NOT NULL PRIMARY KEY,
+	FirstName VARCHAR(45) NOT NULL,
+	LastName VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE ReservationStatus
+(
+	Id INT IDENTITY NOT NULL PRIMARY KEY,
+	Name VARCHAR(45) NOT NULL UNIQUE
+);
+
+CREATE TABLE PaymentType
+(
+	Id INT IDENTITY NOT NULL PRIMARY KEY,
+	Type VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Preference
+(
+	Id INT IDENTITY NOT NULL PRIMARY KEY,
+	Name VARCHAR(45) NOT NULL UNIQUE
+);
+
+CREATE TABLE ReservationPreference
+(
+	Id INT IDENTITY NOT NULL PRIMARY KEY,
+	PreferenceId INT NOT NULL,
+	Value VARCHAR(45) NOT NULL
+	FOREIGN KEY (PreferenceId) REFERENCES Preference (Id)
+);
+
+CREATE TABLE Customer
+(
+	Id INT IDENTITY PRIMARY KEY,
+	Name VARCHAR(45) NOT NULL,
+	PhoneNumber VARCHAR(20) NOT NULL,
+	City VARCHAR(20) NOT NULL,
+	[State] VARCHAR(45) NOT NULL,
+	Country VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE PaymentMethod
+(
+	Id INT IDENTITY PRIMARY KEY,
+	AccountNumber VARCHAR(255) NOT NULL,
+	CustomerId INT NOT NULL,
+	PaymentTypeId INT NOT NULL,
+	IsPreferred BIT NOT NULL DEFAULT '0',
+	FOREIGN KEY (CustomerId) REFERENCES Customer (Id),
+	FOREIGN KEY (PaymentTypeId) REFERENCES PaymentType (Id)
+);
+
+CREATE TABLE FrequentFlyer
+(
+	FrequentFlyerNumber INT IDENTITY PRIMARY KEY,
+	CustomerId INTEGER NOT NULL
+	FOREIGN KEY (CustomerId) REFERENCES Customer (Id)
+);
+
+CREATE TABLE Flight
+(
+	FlightNumber VARCHAR(45) PRIMARY KEY,
+	DepartureAirportCode VARCHAR(5) NOT NULL,
+	ArrivalAirportCode VARCHAR (5) NOT NULL,
+	AirplaneId INT NOT NULL,
+	CarrierName VARCHAR (45) NOT NULL,
+	[Date] DATE NOT NULL,
+	Fare DECIMAL (10, 2) NOT NULL,
+	DepartureTime TIME NOT NULL,
+	ArrivalTime TIME NOT NULL,
+	FOREIGN KEY (DepartureAirportCode) REFERENCES Airport(InternationalCode),
+	FOREIGN KEY (ArrivalAirportCode) REFERENCES Airport(InternationalCode),
+	FOREIGN KEY (CarrierName) REFERENCES Carrier(Name)
+);
+
+CREATE TABLE Reservation
+(
+	Id INT IDENTITY NOT NULL PRIMARY KEY,
+	CustomerId INT NOT NULL,
+	AgentId INT NOT NULL,
+	ReservationStatusId INT NOT NULL,
+	FrequentFlyerNumber INT NOT NULL,
+	PaymentMethodId INT NOT NULL,
+	FOREIGN KEY (CustomerId) REFERENCES Customer (Id),
+	FOREIGN KEY (AgentId) REFERENCES Agent (Id),
+	FOREIGN KEY (ReservationStatusId) REFERENCES ReservationStatus(Id),
+	FOREIGN KEY (PaymentMethodId) REFERENCES PaymentMethod(Id)
+);
+
+CREATE TABLE Flight_Reservation
+(
+	ReservationId INT NOT NULL,
+	FlightNumber VARCHAR(45) NOT NULL,
+	ReservationStatusId INT NOT NULL,
+	FOREIGN KEY (ReservationId) REFERENCES Reservation (Id),
+	FOREIGN KEY (FlightNumber) REFERENCES Flight(FlightNumber),
+	FOREIGN KEY (ReservationStatusId) REFERENCES ReservationStatus(Id)
+);
